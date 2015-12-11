aa546254c06923cd63471fac6b20a512423d7651
Quinten Clause <quintenclause@hotmail.com>
2015-12-11 19:28:15 +0100

    Initial Commit

---
+++ StarSaver/StarSaverView.h
@@ -0,0 +1,13 @@
+//
+//  StarSaverView.h
+//  StarSaver
+//
+//  Created by Quinten Clause on 11/12/15.
+//  Copyright (c) 2015 Quinten Clause. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface StarSaverView : ScreenSaverView
+
+@end
---
+++ StarSaver/StarSaverView.m
@@ -0,0 +1,52 @@
+//
+//  StarSaverView.m
+//  StarSaver
+//
+//  Created by Quinten Clause on 11/12/15.
+//  Copyright (c) 2015 Quinten Clause. All rights reserved.
+//
+
+#import "StarSaverView.h"
+
+@implementation StarSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
